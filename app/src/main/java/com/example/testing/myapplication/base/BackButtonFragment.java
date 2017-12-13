23d269d751d25828979dc98585ef786f273bd5f9
Jubril Edu <djubreel@gmail.com>
2017-12-13 13:21:06 +0100

    Implemented build distribution using crashlytics.

---
+++ app/src/main/java/com/example/testing/myapplication/base/BackButtonFragment.java
@@ -0,0 +1,27 @@
+package com.example.testing.myapplication.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+
+import com.cottacush.android.libraries.base.BaseActivity;
+
+
+/**
+ * Created by codedentwickler on 10/6/17.
+ */
+
+public abstract class BackButtonFragment<T extends BasePresenter> extends BaseFragment<T> {
+    com.cottacush.android.libraries.base.BaseActivity baseActivity;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        baseActivity = ((BaseActivity) getActivity());
+        ActionBar actionBar = baseActivity.getSupportActionBar();
+        if (actionBar != null) {
+            baseActivity.setDrawerIconToBack();
+        }
+    }
+}
