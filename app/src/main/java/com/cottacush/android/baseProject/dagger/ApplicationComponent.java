8da30a9dd8621b71e08a377f94750ab9148e8aef
John <itoohue@gmail.com>
2018-09-06 15:15:25 +0100

    Added Dagger dependencies and created skeleton to enable use of DI in new in-house Android projects

--- app/src/main/java/com/cottacush/android/baseProject/dagger/ApplicationComponent.java
+++ app/src/main/java/com/cottacush/android/baseProject/dagger/ApplicationComponent.java
@@ -1,4 +1,14 @@
 package com.cottacush.android.baseProject.dagger;
 
+
+import com.cottacush.android.baseProject.dagger.modules.ApplicationModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {ApplicationModule.class})
 public interface ApplicationComponent {
+    // specify injection target(s) or expose downStream Components here.
 }
