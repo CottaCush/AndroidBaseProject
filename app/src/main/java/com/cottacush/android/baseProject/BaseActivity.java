81f2ae7ef8da7f5d281a8c3c760e1e7f3387e0ba
John <itoohue@gmail.com>
2018-12-05 15:24:40 +0100

    Fixed Style Issues

--- app/src/main/java/com/cottacush/android/baseProject/BaseActivity.java
+++ app/src/main/java/com/cottacush/android/baseProject/BaseActivity.java
@@ -127,7 +127,8 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
         // Check if no view has focus:
         View view = this.getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
--- app/src/test/java/com/cottacush/android/baseProject/ExampleUnitTest.java
+++ app/src/test/java/com/cottacush/android/baseProject/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.cottacush.android.baseProject;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
